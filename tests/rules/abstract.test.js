4d77c51ea5ac73796d19e086987a003ca2afd5e3
Julia Lind <lind.julia@hotmail.com>
2025-11-15 23:01:47 +0100

    Add unittests to ensure that abstract classes cannot be instantiated directly RED

---
+++ tests/rules/abstract.test.js
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import { SameOfAKind } from '../../src/rules/SameOfAKind.js'
+import { Rule } from '../../src/rules/Rule.js'
+
+
+
+describe('Abstract classes should not be instantiable', () => {
+  it ('Instantiating SameOfAKind should throw an error', () => {
+    expect(() => new SameOfAKind(2)).toThrowError(Error)
+  })
+
+  it ('Instantiating Rule should throw an error', () => {
+    expect(() => new Rule(2)).toThrowError(Error)
+  })
+})
