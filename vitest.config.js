18db269cc22f1e84a93ddc954a9ccfe31a93d70c
Julia Lind <lind.julia@hotmail.com>
2025-11-15 19:18:37 +0100

    Update cofnig

--- vitest.config.js
+++ vitest.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['tests/**/*.test.js'],
     coverage: {
       provider: 'v8',
-      reporter: ['text'],
-      reportsDirectory: null,
+      reporter: ['text'], // show coverage report only in the terminal
+      all: true, // to include files with 0 coverage in the coverage report
+      include: ['src/**/*.js'],
+      exclude: [
+        'node_modules/**',
+        'tests/**',
+      ],
     },
   },
 })
