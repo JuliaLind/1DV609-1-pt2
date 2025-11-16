85e28e51c9e5bcc25a28ed6898ddbf45be1f7a2c
Julia Lind <lind.julia@hotmail.com>
2025-11-16 20:21:27 +0100

    Add unittests for Straight.test() method RED

--- tests/rules/Straight.test.js
+++ tests/rules/Straight.test.js
@@ -13,4 +13,46 @@ describe('Straight', () => {
   it('toString should return "Straight"', () => {
     expect(sut.toString()).toBe('Straight')
   })
+
+  describe('test()', () => {
+    const threeHearts = Object.freeze({
+      getValue: () => 3
+    })
+
+
+    const eightClubs = Object.freeze({
+      getValue: () => 8
+    })
+
+    const nineDiamonds = Object.freeze({
+      getValue: () => 9
+    })
+
+    const tenSpades = Object.freeze({
+      getValue: () => 10
+    })
+
+    const jackHearts = Object.freeze({
+      getValue: () => 11
+    })
+
+    const queenClubs = Object.freeze({
+      getValue: () => 12
+    })
+
+    it('should return true for ranks [Q, 10, J, 8, 9]', () => {
+        const actual = sut.test([queenClubs, tenSpades, jackHearts, eightClubs, nineDiamonds])
+        expect(actual).toBe(true)
+    })
+
+    it('should return false for ranks [3, 10, J, 8, 9]', () => {
+        const actual = sut.test([threeHearts, tenSpades, jackHearts, eightClubs, nineDiamonds])
+        expect(actual).toBe(false)
+    })
+
+    it('should return false for ranks [Q, 10, J, 9]', () => {
+        const actual = sut.test([queenClubs, tenSpades, jackHearts, nineDiamonds, undefined])
+        expect(actual).toBe(false)
+    })
+  })
 })
