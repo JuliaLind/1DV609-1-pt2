48ae6396df2ca98fbe5af148c37c77c1a35793b1
Julia Lind <lind.julia@hotmail.com>
2025-11-16 20:53:59 +0100

    Implement Straight.test() method GREEN

--- src/rules/Straight.js
+++ src/rules/Straight.js
@@ -3,7 +3,26 @@ import { Rule } from './Rule.js'
 export class Straight extends Rule {
   constructor() {
     const value = 15
-    const name = 'Straight' 
+    const name = 'Straight'
     super(value, name)
+  }
+
+  test(cards) {
+    if (cards.includes(undefined)) {
+      return false
+    }
+
+    return this.#areSequential(cards)
+  }
+
+  #areSequential(cards) {
+    const sortedCards = this.#sortLowestToHighest(cards)
+    const lastIndex = sortedCards.length - 1
+
+    return sortedCards[lastIndex].getValue() - sortedCards[0].getValue() === 4
+  }
+
+  #sortLowestToHighest(cards) {
+    return [...cards].sort((a, b) => a.getValue() - b.getValue())
   }
 }
