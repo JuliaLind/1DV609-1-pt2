9ce6db9016e359b729359dce2e09dc07fb9e710b
Julia Lind <lind.julia@hotmail.com>
2025-11-16 23:53:46 +0100

    Implement getDistinctValues method GREEN

--- src/logic/CardCollection.js
+++ src/logic/CardCollection.js
@@ -24,4 +24,21 @@ export class CardCollection {
   hasRank(rank) {
     return this.#cards.some(card => card?.getAttribute('rank') === rank)
   }
+
+  getDistinctValues() {
+    const values = []
+
+    for (const card of this.#cards) {
+      const value = card?.getValue()
+
+      if (value && !values.includes(value)) {
+        values.push(value)
+      }
+    }
+    return this.#sortLowestToHighest(values)
+  }
+
+  #sortLowestToHighest(values) {
+    return [...values].sort((a, b) => a - b)
+  }
 }
