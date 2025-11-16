ac69072211d65e8dd5691105205a2689740d9dcd
Julia Lind <lind.julia@hotmail.com>
2025-11-16 23:42:38 +0100

    Add unittest for CardCollection.getDistinctValues method

--- tests/logic/CardCollection.test.js
+++ tests/logic/CardCollection.test.js
@@ -3,16 +3,28 @@ import { CardCollection } from '../../src/logic/CardCollection.js'
 import { getAttributeMock } from '../lib/helpers.js'
 
 describe('CardCollection', () => {
+  const eightHearts = Object.freeze({
+    getAttribute: getAttributeMock({ 'rank': '8', 'suit': 'hearts' }),
+    getValue: () => 8
+  })
+
+  const eightClubs = Object.freeze({
+    getAttribute: getAttributeMock({ 'rank': '8', 'suit': 'clubs' }),
+    getValue: () => 8
+  })
+
   const jackHearts = Object.freeze({
     getAttribute: getAttributeMock({ 'rank': 'J', 'suit': 'hearts' }),
     getValue: () => 11
   })
 
-  it('addCard() should add a card at the specified position', () => {
-    const sut = new CardCollection(5)
-    sut.addCard(jackHearts, 2)
+  describe('addCard()', () => {
+    it('addCard() should add a card at the specified position if the slot is empty', () => {
+      const sut = new CardCollection(5)
+      sut.addCard(jackHearts, 2)
 
-    expect(sut.getCards()).toEqual([undefined, undefined, jackHearts, undefined, undefined])
+      expect(sut.getCards()).toEqual([undefined, undefined, jackHearts, undefined, undefined])
+    })
   })
 
   describe('CardCollection with  [,,J♥,,]', () => {
@@ -37,6 +49,21 @@ describe('CardCollection', () => {
 
     it('hasRank("A") should return false for collection [,,J♥,,]', () => {
       expect(sut.hasRank('A')).toBe(false)
+    })
+  })
+
+  describe('CardCollection with [,J♥,,8♥,8♣]', () => {
+    let sut
+
+    beforeEach(() => {
+      sut = new CardCollection(5)
+      sut.addCard(jackHearts, 1)
+      sut.addCard(eightHearts, 3)
+      sut.addCard(eightClubs, 4)
+    })
+
+    it ('getDistinctValues() should return [8,11]', () => {
+      expect(sut.getDistinctValues()).toEqual([8,11])
     })
   })
 })
