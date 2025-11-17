c47869be7fc4280c2e41c4bd4ace241342b43294
Julia Lind <lind.julia@hotmail.com>
2025-11-17 01:22:48 +0100

    Add unittests for Flush.test method RED

--- tests/rules/Flush.test.js
+++ tests/rules/Flush.test.js
@@ -3,11 +3,40 @@ import { Flush } from '../../src/rules/Flush.js'
 
 describe('Flush', () => {
   const sut = new Flush()
-    it('getValue should return 20', () => {
+  it('getValue should return 20', () => {
     expect(sut.getValue()).toBe(20)
   })
 
   it('toString should return "Flush"', () => {
     expect(sut.toString()).toBe('Flush')
+  })
+
+  describe('test()', () => {
+    it('should return true for same suite cards', () => {
+      const cardCollection = {
+        hasEmptySlots: () => false,
+        isSameSuite: () => true,
+      }
+      const actual = sut.test(cardCollection)
+      expect(actual).toBe(true)
+    })
+
+    it('should return false for different suite cards', () => {
+      const cardCollection = {
+        hasEmptySlots: () => false,
+        isSameSuite: () => false,
+      }
+      const actual = sut.test(cardCollection)
+      expect(actual).toBe(false)
+    })
+
+    it('should return false for cards with empty slots', () => {
+      const cardCollection = {
+        hasEmptySlots: () => true,
+        isSameSuite: () => true,
+      }
+      const actual = sut.test(cardCollection)
+      expect(actual).toBe(false)
+    })
   })
 })
