1c5260854e1d67a5b7425b1debac36fab4f15630
Julia Lind <lind.julia@hotmail.com>
2025-11-17 01:29:50 +0100

    Implement FourOfAKind class GREEN

---
+++ src/rules/FourOfAKind.js
@@ -0,0 +1,15 @@
+import { SameOfAKind } from './SameOfAKind.js'
+
+export class FourOfAKind extends SameOfAKind {  
+  constructor() {
+    const value = 50
+    const name = 'Four of a Kind'
+    const rankCount = 4
+
+    super(value, name, rankCount)
+  }
+
+  test(cards) {
+    return super._test(cards)
+  }
+}
