381e328ab5e29c66972455969077c43ade3df4cc
Julia Lind <lind.julia@hotmail.com>
2025-11-19 00:44:48 +0100

    Create test for innterhtml of the poker-grid RED

--- tests/components/poker-grid.test.js
+++ tests/components/poker-grid.test.js
@@ -5,4 +5,13 @@ describe('poker-grid', () => {
   it('poker-grid should be defined as a custom element', () => {
     expect(customElements.get('poker-grid')).toBeDefined()
   })
+
+  it('poker-grid should contain slots for cards and for results', () => {
+    const pokerGrid = document.createElement('poker-grid')
+    const shadowRoot = pokerGrid.shadowRoot
+    expect(shadowRoot.querySelectorAll('.card-slot').length).toBe(25)
+    expect(shadowRoot.querySelectorAll('.result-slot').length).toBe(11)
+    expect(shadowRoot.querySelectorAll('[data-row="0"]').length).toBe(6)
+    expect(shadowRoot.querySelectorAll('[data-column="0"]').length).toBe(6)
+  })
 })
