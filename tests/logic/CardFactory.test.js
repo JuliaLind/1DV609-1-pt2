0d5902a0c3f7395f14c8d970a51b585ed524c6a1
Julia Lind <lind.julia@hotmail.com>
2025-11-22 01:53:23 +0100

    Create testsuite for CardFactory class RED

---
+++ tests/logic/CardFactory.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { CardFactory } from '../../src/logic/CardFactory.js'
+
+describe('CardFactory', () => {
+  it('createCard() should create a poker-card element with correct attributes', () => {
+    const sut = new CardFactory()
+    const card = sut.createCard('hearts', 'A')
+    expect(card.tagName.toLowerCase()).toBe('poker-card')
+    expect(card.getAttribute('suite')).toBe('hearts')
+    expect(card.getAttribute('rank')).toBe('A')
+  })
+})
