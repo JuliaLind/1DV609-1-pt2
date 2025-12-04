0b1e9e5c9385ea003e5be46812da6107d245f427
Julia Lind <lind.julia@hotmail.com>
2025-12-04 12:40:10 +0100

    Ensure FourOfAKind has correct name and points GREEN

--- src/js/rules/FourOfAKind.js
+++ src/js/rules/FourOfAKind.js
@@ -1,12 +1,17 @@
-import { SameOfAKind } from "./SameOfAKind.js"
+import { SameOfAKind } from './SameOfAKind.js'
 
 /**
  * Class representing the Four Of A Kind rule.
  */
 export class FourOfAKind extends SameOfAKind {
-  constructor() {
+  /**
+   * Creates an instance of FourOfAKind class.
+   */
+  constructor () {
     const sameRankCount = 4
+    const name = 'Four Of A Kind'
+    const value = 50
 
-    super(sameRankCount)
+    super(sameRankCount, name, value)
   }
 }
