bce42dc883440a74ccbbd056bafe72f4474bf06f
Julia Lind <lind.julia@hotmail.com>
2025-12-04 13:54:42 +0100

    Add first unittest for FullHouse class RED

---
+++ tests/rules/FullHouse.test.js
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { FullHouse } from '../../src/js/rules/FullHouse.js'
+
+describe('FullHouse', () => {
+  it('toObject should return {name: "Full House", points: 25 }', () => {
+    const sut = new FullHouse()
+    const expected = {
+      name: 'Full House',
+      points: 25
+    }
+
+    expect(sut.toObject()).toEqual(expected)
+  })
+})
