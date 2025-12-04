d3619be9d263abd345be6e3d1f221a7b7fe64645
Julia Lind <lind.julia@hotmail.com>
2025-12-04 18:39:01 +0100

    Implement RoyalFlush.test method so it passes the unittest GREEN

--- src/js/rules/RoyalFlush.js
+++ src/js/rules/RoyalFlush.js
@@ -1,16 +1,33 @@
 import { Rule } from './Rule.js'
+import { StraightFlush } from './StraightFlush.js'
 
 /**
  * Class representing the Flush rule.
  */
 export class RoyalFlush extends Rule {
+  #straightFlushRule
+
   /**
    * Creates an instance of Royal Flush rule.
+   *
+   * @param {StraightFlush} straightFlush - an instance of the StraightFlush rule
    */
-  constructor () {
+  constructor (straightFlush = new StraightFlush()) {
     const name = 'Royal Flush'
     const value = 100
 
     super(name, value)
+
+    this.#straightFlushRule = straightFlush
+  }
+
+  /**
+   * Tests if the given line satisfies the Royal Flush rule.
+   *
+   * @param {object} line - the line of cards to test
+   * @returns { boolean } - true if the line satisfies the Royal Flush rule, false otherwise
+   */
+  test (line) {
+    return this.#straightFlushRule.test(line)
   }
 }
