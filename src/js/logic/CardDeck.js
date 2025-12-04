dcb33bf6ebc5d73837f1a2964e167d4cce0cc6ae
Julia Lind <lind.julia@hotmail.com>
2025-12-05 00:53:56 +0100

    Adjust implementation of CardDeck to pass the shuffle unittest GREEN

--- src/js/logic/CardDeck.js
+++ src/js/logic/CardDeck.js
@@ -1,10 +1,11 @@
 import { CardFactory } from './CardFactory.js'
+import { Card } from './Card.js'
 
 /**
  * Represents a deck of cards.
  */
 export class CardDeck {
-  #cards
+  #cards = []
 
   /**
    * Creates an instance of CardDeck.
@@ -12,7 +13,40 @@ export class CardDeck {
    * @param {CardFactory} cardFactory - the factory for creating cards for the deck
    */
   constructor (cardFactory = new CardFactory()) {
-    this.#cards = cardFactory.createCards()
+    this.#cards = [...cardFactory.createCards()]
+    this.#shuffleInPlace()
+  }
+
+  /**
+   * Shuffles the cards in the deck in place using the Fisher-Yates algorithm.
+   */
+  #shuffleInPlace () {
+    for (let index = this.#cards.length - 1; index > 0; index--) {
+      const randomIndex = this.#selectRandomIndex(index + 1)
+      console.log(`Swapping index ${index} with random index ${randomIndex}`)
+
+      this.#swapCards(index, randomIndex)
+    }
+  }
+
+  /**
+   * Selects a random index up to the given upper limit.
+   *
+   * @param {number} upperLimit - the upper limit (exclusive) for the random index
+   * @returns {number} - the selected random index
+   */
+  #selectRandomIndex (upperLimit) {
+    return Math.floor(Math.random() * upperLimit)
+  }
+
+  /**
+   * Swaps two cards in the deck.
+   *
+   * @param {number} index1 - index position of first card
+   * @param {number} index2 - index position of second card
+   */
+  #swapCards (index1, index2) {
+    [this.#cards[index1], this.#cards[index2]] = [this.#cards[index2], this.#cards[index1]]
   }
 
   /**
