4f6d75df3cd33f91fee5c7494b7b7160beb9ff9b
Julia Lind <lind.julia@hotmail.com>
2025-12-04 17:05:50 +0100

    Implement testing for consecutive cards + REFACTOR, GREEN

--- src/js/rules/Straight.js
+++ src/js/rules/Straight.js
@@ -24,10 +24,49 @@ export class Straight extends Rule {
     const rankFrequencies = line.getRankFrequencies()
     const uniqueRanks = Object.keys(rankFrequencies)
 
-    if (uniqueRanks.length !== 5) {
-      return false
-    }
+    return this.#hasFiveUniqueRanks(uniqueRanks) && this.#isConsecutive(uniqueRanks)
+  }
 
-    return true
+  /**
+   * Returns true if the array contains five unique ranks.
+   *
+   * @param {number[]} uniqueRanks - checks it the array contains 5 items
+   * @returns {boolean} - true if the array contains 5 items, false otherwise
+   */
+  #hasFiveUniqueRanks (uniqueRanks) {
+    return uniqueRanks.length === 5
+  }
+
+  /**
+   * Checks if the given ranks are consecutive.
+   *
+   * @param {number[]} ranks - the array of ranks
+   * @returns {boolean} - true if the ranks are consecutive, false otherwise
+   */
+  #isConsecutive (ranks) {
+    const highestRank = this.#getHighestRank(ranks)
+    const lowestRank = this.#getLowestRank(ranks)
+
+    return highestRank - lowestRank === ranks.length - 1
+  }
+
+  /**
+   * Returns the highest rank from the given ranks.
+   *
+   * @param {number[]} ranks - array of ranks
+   * @returns {number} - highest rank
+   */
+  #getHighestRank (ranks) {
+    return Math.max(...ranks)
+  }
+
+  /**
+   * Reurns the lowest rank from the given ranks.
+   *
+   * @param {number[]} ranks - array of ranks
+   * @returns {number} - lowest rank
+   */
+  #getLowestRank (ranks) {
+    return Math.min(...ranks)
   }
 }
