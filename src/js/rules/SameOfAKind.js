b664aa22b90fe5c0cba29ceb7095e2794296c720
Julia Lind <lind.julia@hotmail.com>
2025-12-04 02:43:53 +0100

    Implement SameOfAKind.test method GREEN

--- src/js/rules/SameOfAKind.js
+++ src/js/rules/SameOfAKind.js
@@ -1,7 +1,26 @@
 export class SameOfAKind {
-  constructor() {
+  #rankCount
+
+  constructor(rankCount) {
     if (new.target === SameOfAKind) {
       throw new Error('SameOfAKind is an abstract class')
+    }
+
+    this.#setRankCount(rankCount)
+  }
+
+  #setRankCount(rankCount) {
+    this.#rankCount = rankCount
+  }
+
+  test(line) {
+    const rankFrequencies = line.getRankFrequencies()
+    const frequencies = Object.values(rankFrequencies)
+
+    for (const frequency of frequencies) {
+      if (frequency >= this.#rankCount) {
+        return true
+      }
     }
   }
 }
