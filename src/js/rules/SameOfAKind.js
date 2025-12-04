028e6a95a3ebfde40cda65c4440fcd8d9655ed9d
Julia Lind <lind.julia@hotmail.com>
2025-12-04 02:53:35 +0100

    Add function docstrings

--- src/js/rules/SameOfAKind.js
+++ src/js/rules/SameOfAKind.js
@@ -1,6 +1,15 @@
+/**
+ * Abstract class representing a rule that checks
+ * for a certain number of cards of the same rank.
+ */
 export class SameOfAKind {
   #rankCount
 
+  /**
+   * Creates an instance of SameOfAKind.
+   * 
+   * @param {number} rankCount - the number of same rank cards required to fulfill the rule
+   */
   constructor(rankCount) {
     if (new.target === SameOfAKind) {
       throw new Error('SameOfAKind is an abstract class')
@@ -9,10 +18,21 @@ export class SameOfAKind {
     this.#setRankCount(rankCount)
   }
 
+  /**
+   * Sets the rank count required for the rule.
+   *
+   * @param {number} rankCount - the number of cards required of the same rank
+   */
   #setRankCount(rankCount) {
     this.#rankCount = rankCount
   }
 
+  /**
+   * Tests if the given line of cards fulfills the rule.
+   *
+   * @param {object} line - the line of cards to test for the rule 
+   * @returns {boolean} - true if the line fulfills the rule, false otherwise
+   */
   test(line) {
     const rankFrequencies = line.getRankFrequencies()
     const frequencies = Object.values(rankFrequencies)
