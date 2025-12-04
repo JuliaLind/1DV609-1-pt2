587638c652891bb16522c237e9663f0469929636
Julia Lind <lind.julia@hotmail.com>
2025-12-04 17:22:52 +0100

    Add unittest for StraightFlush when line does not have Straight RED

--- tests/rules/StraightFlush.test.js
+++ tests/rules/StraightFlush.test.js
@@ -2,13 +2,23 @@ import { describe, it, expect } from 'vitest'
 import { StraightFlush } from '../../src/js/rules/StraightFlush.js'
 
 describe('StraightFlush', () => {
-  it('toObject should return {name: "Straight Flush", points: 75 }', () => {
-    const sut = new StraightFlush()
-    const expected = {
-      name: 'Straight Flush',
-      points: 75
-    }
+    it('toObject should return {name: "Straight Flush", points: 75 }', () => {
+        const sut = new StraightFlush()
+        const expected = {
+            name: 'Straight Flush',
+            points: 75
+        }
 
-    expect(sut.toObject()).toEqual(expected)
-  })
+        expect(sut.toObject()).toEqual(expected)
+    })
+
+    it('StraightFlush.test() should return false if Straight.test returns false', () => {
+        const straightMock = {
+            test: () => false
+        }
+
+        const sut = new StraightFlush(straightMock)
+
+        expect(sut.test()).toBe(false)
+    })
 })
