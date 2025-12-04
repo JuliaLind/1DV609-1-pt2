ce0fb81c2e1c391e9cf8dcc879fc283c084ddb7d
Julia Lind <lind.julia@hotmail.com>
2025-12-04 02:05:52 +0100

    Implement Rule.toObject method GREEN

--- src/js/rules/Rule.js
+++ src/js/rules/Rule.js
@@ -2,14 +2,32 @@
  * Abstract base class for all rules.
  */
 export class Rule {
+  #name = ''
+  #value = 0
+
   /**
    * Creates an instance of Rule.
    *
    * @throws {Error} If instantiated directly.
    */
-  constructor() {
+  constructor(name = '', value = 0) {
     if (new.target === Rule) {
       throw new Error('Rule is an abstract class')
+    }
+
+    this.#name = name
+    this.#value = value
+  }
+
+  /**
+   * Returns an plain object representation of the rule.
+   * 
+   * @returns {Object} - an associative array with name and points of the rule
+   */
+  toObject() {
+    return {
+      name: this.#name,
+      points: this.#value
     }
   }
 }
