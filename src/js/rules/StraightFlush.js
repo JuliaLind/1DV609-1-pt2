e324c6751f8b68d8740a166959c4ebddb7de6ff8
Julia Lind <lind.julia@hotmail.com>
2025-12-04 17:30:41 +0100

    Create StraightFlush.test implementation that returns false if the line does not contain a straight GREEN

--- src/js/rules/StraightFlush.js
+++ src/js/rules/StraightFlush.js
@@ -1,16 +1,33 @@
 import { Rule } from './Rule.js'
+import { Straight } from './Straight.js'
 
 /**
  * Class representing the Straight Flush rule.
  */
 export class StraightFlush extends Rule {
+  #straightRule
+
   /**
    * Creates an instance of StraightFlush class.
+   *
+   * @param {Straight} straightRule - an instance of the Straight rule
    */
-  constructor () {
+  constructor (straightRule = new Straight()) {
     const name = 'Straight Flush'
     const value = 75
 
     super(name, value)
+
+    this.#straightRule = straightRule
+  }
+
+  /**
+   * Tests whether the given line contains five cards of consecutive ranks and same suit.
+   *
+   * @param {object} line - the line of cards to test for StraightFlush rule
+   * @returns { boolean } - true if the line contains a straight flush, false otherwise
+   */
+  test (line) {
+    return this.#straightRule.test(line)
   }
 }
--- tests/rules/StraightFlush.test.js
+++ tests/rules/StraightFlush.test.js
@@ -2,23 +2,28 @@ import { describe, it, expect } from 'vitest'
 import { StraightFlush } from '../../src/js/rules/StraightFlush.js'
 
 describe('StraightFlush', () => {
-    it('toObject should return {name: "Straight Flush", points: 75 }', () => {
-        const sut = new StraightFlush()
-        const expected = {
-            name: 'Straight Flush',
-            points: 75
-        }
+  it('toObject should return {name: "Straight Flush", points: 75 }', () => {
+    const sut = new StraightFlush()
+    const expected = {
+      name: 'Straight Flush',
+      points: 75
+    }
 
-        expect(sut.toObject()).toEqual(expected)
-    })
+    expect(sut.toObject()).toEqual(expected)
+  })
 
-    it('StraightFlush.test() should return false if Straight.test returns false', () => {
-        const straightMock = {
-            test: () => false
-        }
+  it('StraightFlush.test() should return false if Straight.test returns false', () => {
+    const straightMock = {
+      /**
+       * Mock method for test.
+       *
+       * @returns {boolean} - always returns false
+       */
+      test: () => false
+    }
 
-        const sut = new StraightFlush(straightMock)
+    const sut = new StraightFlush(straightMock)
 
-        expect(sut.test()).toBe(false)
-    })
+    expect(sut.test()).toBe(false)
+  })
 })
