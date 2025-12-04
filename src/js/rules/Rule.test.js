508d0c9e956a56ff38ccef9921d48e949b5640b8
Julia Lind <lind.julia@hotmail.com>
2025-12-04 01:49:21 +0100

    Create first unittest for Rule class RED

---
+++ src/js/rules/Rule.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+import { Rule } from '../../src/js/rules/Rule.js'
+
+
+describe('Rule', () => {
+  it('Rule should be abstract - instantiating directly should throw an error', () => {
+    expect(() => new Rule()).toThrowError(Error)
+  })
+})
