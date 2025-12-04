4f06d63a68b6758dd902756e612ce5e5e1247f21
Julia Lind <lind.julia@hotmail.com>
2025-12-04 22:32:21 +0100

    Create Card class GREEN

---
+++ src/js/logic/Card.js
@@ -0,0 +1,30 @@
+/**
+ * Represents the logic of a playing card.
+ */
+export class Card {
+  #rank
+  #suite
+
+  /**
+   * Creates an instance of Card.
+   *
+   * @param {string} rank - the rank of the card
+   * @param {string} suite - the suite of the card
+   */
+  constructor (rank, suite) {
+    this.#rank = rank
+    this.#suite = suite
+  }
+
+  /**
+   * Returns plain object representation of the current instance.
+   *
+   * @returns {object} - associative array containing the rank and suite of the current instance.
+   */
+  toObject () {
+    return {
+      rank: this.#rank,
+      suite: this.#suite
+    }
+  }
+}
