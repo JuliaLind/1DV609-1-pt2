f3ca0b61b97fe67af99e1a12ef43740109096240
Julia Lind <lind.julia@hotmail.com>
2025-12-04 10:34:21 +0100

    Create ThreeOfAKind class GREEN

---
+++ src/js/rules/ThreeOfAKind.js
@@ -0,0 +1,17 @@
+import { SameOfAKind } from "./SameOfAKind.js"
+
+/**
+ * Class representing the Three Of A Kind rule.
+ */
+export class ThreeOfAKind extends SameOfAKind {
+  /**
+   * Creates an instance of ThreeOfAKind class.
+   */
+  constructor() {
+    const sameRankCount = 3
+    const name = 'Three Of A Kind'
+    const value = 10
+
+    super(sameRankCount, name, value)
+  }
+}
