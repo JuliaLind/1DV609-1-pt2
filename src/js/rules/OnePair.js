6b9982df471d87892a9f5c49ff90bb9050f3d83b
Julia Lind <lind.julia@hotmail.com>
2025-12-04 02:22:13 +0100

    Create OnePair class GREEN

---
+++ src/js/rules/OnePair.js
@@ -0,0 +1,13 @@
+import { Rule } from './Rule.js'
+
+/**
+ * Class representing the One Pair rule.
+ */
+export class OnePair extends Rule {
+  /**
+   * Creates an instance of OnePair rule.
+   */
+  constructor() {
+    super('One Pair', 2)
+  }
+}
