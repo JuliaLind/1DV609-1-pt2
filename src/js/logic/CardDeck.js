e9e2c3d9e8ea660b5f32c374f19633787f6dff57
Julia Lind <lind.julia@hotmail.com>
2025-12-05 00:05:58 +0100

    Create CardDeck class GREEN

---
+++ src/js/logic/CardDeck.js
@@ -0,0 +1,17 @@
+import { CardFactory } from './CardFactory.js'
+
+/**
+ * Represents a deck of cards.
+ */
+export class CardDeck {
+  #cards
+
+  /**
+   * Creates an instance of CardDeck.
+   *
+   * @param {CardFactory} cardFactory - the factory for creating cards for the deck
+   */
+  constructor (cardFactory = new CardFactory()) {
+    this.#cards = cardFactory.createCards()
+  }
+}
--- tests/logic/CardDeck.test.js
+++ tests/logic/CardDeck.test.js
@@ -1,13 +1,13 @@
-import { describe, it, expect,vi } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { CardDeck } from '../../src/js/logic/CardDeck.js'
 
 describe('CardDeck', () => {
   it('CardDeck constructor should call the CardFactory.createCards method', () => {
     const cardFactoryMock = {
-        createCards: vi.fn()
+      createCards: vi.fn()
     }
     new CardDeck(cardFactoryMock) // eslint-disable-line no-new
-    
+
     expect(cardFactoryMock.createCards).toHaveBeenCalledOnce()
   })
 })
