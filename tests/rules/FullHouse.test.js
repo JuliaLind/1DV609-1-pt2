75969b00e48344166a8771e62058126a84e2257c
Julia Lind <lind.julia@hotmail.com>
2025-12-04 18:00:54 +0100

    Fix linting errors, add testcase for FullHouse.test happy path GREEN

--- tests/rules/FullHouse.test.js
+++ tests/rules/FullHouse.test.js
@@ -54,4 +54,30 @@ describe('FullHouse', () => {
 
     expect(sut.test(lineStub)).toBe(false)
   })
+
+  it('FullHouse.test() should return true if both ThreeOfAKind.test and OnePair.test return true', () => {
+    const onePairStub = {
+      /**
+       * Stub method for test.
+       *
+       * @returns {boolean} - always returns true
+       */
+      test: () => true
+    }
+
+    const threeOfAKindStub = {
+      /**
+       * Stub method for test.
+       *
+       * @returns {boolean} - always returns true
+       */
+      test: () => true
+    }
+
+    const sut = new FullHouse(onePairStub, threeOfAKindStub)
+
+    const lineStub = {}
+
+    expect(sut.test(lineStub)).toBe(true)
+  })
 })
