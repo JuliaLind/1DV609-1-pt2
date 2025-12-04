15141d817fd1f30fdb0772064250bbf9898b9df3
Julia Lind <lind.julia@hotmail.com>
2025-12-04 17:58:29 +0100

    Update implementation of FullHouse to also rely on ThreeOfAKind class GREEN

--- src/js/rules/FullHouse.js
+++ src/js/rules/FullHouse.js
@@ -1,24 +1,28 @@
 import { Rule } from './Rule.js'
 import { OnePair } from './OnePair.js'
+import { ThreeOfAKind } from './ThreeOfAKind.js'
 
 /**
  * Class representing the Full House rule.
  */
 export class FullHouse extends Rule {
   #onePairRule
+  #threeOfAKindRule
 
   /**
    * Creates an instance of FullHouse class.
    *
    * @param {OnePair} onePairRule - an instance of the OnePair rule
+   * @param {ThreeOfAKind} threeOfAKindRule - an instance of the ThreeOfAKind rule
    */
-  constructor (onePairRule = new OnePair()) {
+  constructor (onePairRule = new OnePair(), threeOfAKindRule = new ThreeOfAKind()) {
     const name = 'Full House'
     const value = 25
 
     super(name, value)
 
     this.#onePairRule = onePairRule
+    this.#threeOfAKindRule = threeOfAKindRule
   }
 
   /**
@@ -28,6 +32,6 @@ export class FullHouse extends Rule {
    * @returns { boolean} - true if the line has a pair of one rank and three of another rank, false otherwise
    */
   test (line) {
-    return this.#onePairRule.test(line)
+    return this.#onePairRule.test(line) && this.#threeOfAKindRule.test(line)
   }
 }
