64b5d0d48a3597a5d37a116879c3ab1c9f88216d
Julia Lind <lind.julia@hotmail.com>
2025-12-06 15:51:11 +0100

    Add validation to GridLine constructor GREEN

--- src/js/logic/GridLine.js
+++ src/js/logic/GridLine.js
@@ -12,7 +12,21 @@ export class GridLine {
    * @param {Card[]} slots - array of cards to initialize the grid line with
    */
   constructor (slots = new Array(5)) {
+    this.#validateLength(slots)
+
     this.#slots = [...slots]
+  }
+
+  /**
+   * Validates the length of the slots array.
+   *
+   * @param {Card[]} slots - array with slots that may contain cards
+   * @throws {Error} - if the length of slots is not exactly 5
+   */
+  #validateLength (slots) {
+    if (slots.length !== 5) {
+      throw new Error('GridLine must have exactly 5 slots')
+    }
   }
 
   /**
