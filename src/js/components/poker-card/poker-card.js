c9c41d7e243a3a4106c87653855fff15eba327ca
Julia Lind <lind.julia@hotmail.com>
2025-12-09 18:11:36 +0100

    Update poker-card to pass unittest GREEN

--- src/js/components/poker-card/poker-card.js
+++ src/js/components/poker-card/poker-card.js
@@ -24,4 +24,51 @@ customElements.define('poker-card',
    * Class representing a poker card custom element.
    */
   class extends HTMLElement {
+    #rank
+    #suite
+    #img
+
+    /**
+     * Creates an instance of poker-card.
+     */
+    constructor() {
+      super()
+
+      this.attachShadow({ mode: 'open' })
+        .append(template.content.cloneNode(true))
+      
+      this.#img = this.shadowRoot.querySelector('img')
+    }
+
+    /**
+     * Returns an array of attribute names to observe for changes.
+     *
+     * @returns {Array} - array of attribute names to observe
+     */
+    static get observedAttributes() {
+      return ['rank', 'suite']
+    }
+
+    /**
+     * Called when one of the observed attributes changes.
+     * Ensures that rank and suite cannot be changed once set.
+     *
+     * @param {string} name - name of the changed attribute
+     * @param {any} oldValue - old value of the changed attribute
+     * @param {any} newValue - new value of the changed attribute
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+      switch (name) {
+        case 'rank':
+          this.#rank = newValue
+
+          this.#img.setAttribute('alt', `${this.#rank} of ${this.#suite}`)
+          break
+        case 'suite':
+          this.#suite = newValue
+
+          this.#img.setAttribute('alt', `${this.#rank} of ${this.#suite}`)
+          break
+      }
+    }
   })
