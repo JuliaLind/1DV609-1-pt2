cd083a77012015f873cfd2682dbd97e06ead84ee
Julia Lind <lind.julia@hotmail.com>
2025-12-09 21:24:22 +0100

    Update poker-game to pass second test GREEN

--- src/js/components/poker-game/poker-game.js
+++ src/js/components/poker-game/poker-game.js
@@ -1,3 +1,6 @@
+import '../poker-card/'
+import { Game } from '../../logic/Game.js'
+
 const template = document.createElement('template')
 
 template.innerHTML = `
@@ -7,10 +10,34 @@ template.innerHTML = `
       box-sizing: border-box;
     }
   </style>
+
+    <div id="next-card"></div>
 `
 
 customElements.define('poker-game',
   /**
    * Represents the poker-game component.
    */
-  class extends HTMLElement {})
+  class extends HTMLElement {
+    #game = new Game()
+    #nextCardSlot
+
+    /**
+     * Creates an instance of poker-game.
+     */
+    constructor () {
+      super()
+      this.attachShadow({ mode: 'open' })
+      this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+      const nextCardSlot = this.shadowRoot.querySelector('#next-card')
+
+      const nextCard = this.#game.getNextCard()
+
+      const card = document.createElement('poker-card')
+      card.setAttribute('suite', nextCard.suite)
+      card.setAttribute('rank', nextCard.rank)
+
+      nextCardSlot.appendChild(card)
+    }
+  })
