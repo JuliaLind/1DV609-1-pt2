9fd79ccd5b399ef0682c3049a074f5b4102c1b3f
Julia Lind <lind.julia@hotmail.com>
2025-12-10 00:41:05 +0100

    Update ThreeOfAKind implementation GREEN

--- src/js/rules/ThreeOfAKind.js
+++ src/js/rules/ThreeOfAKind.js
@@ -4,4 +4,12 @@ import { SameOfAKind } from './SameOfAKind.js'
  * Class representing the Three Of A Kind rule.
  */
 export class ThreeOfAKind extends SameOfAKind {
+  /**
+   * Creates an instance of ThreeOfAKind class.
+   */
+  constructor () {
+    const sameRankCount = 3
+
+    super(sameRankCount)
+  }
 }
