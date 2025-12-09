0fee87e40cee4673bc1702b22a4437ef37c20373
Julia Lind <lind.julia@hotmail.com>
2025-12-09 23:32:52 +0100

    Add another unittest for SameOfAKind constructor RED

--- tests/rules/SameOfAKind.test.js
+++ tests/rules/SameOfAKind.test.js
@@ -5,4 +5,15 @@ describe('SameOfAKind', () => {
   it('Is an abstract class - Instantiating directly should throw an error', () => {
     expect(() => new SameOfAKind()).toThrowError(Error)
   })
+
+  it('Instantiating subclass of SameOfAKind should not throw an error', () => {
+    /**
+     * Subclass of SameOfAKind for testing purposes.
+     */
+    class ConcreteRule extends SameOfAKind {
+    }
+
+    expect(() => new ConcreteRule()).not.toThrow()
+  })
+
 })
