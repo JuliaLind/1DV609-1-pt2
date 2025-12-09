ebbea99f5afd74d531b6466b0bad3fdbaa33d358
Julia Lind <lind.julia@hotmail.com>
2025-12-10 00:49:41 +0100

    Add constructor to FourOfAKind GREEN

--- src/js/rules/FourOfAKind.js
+++ src/js/rules/FourOfAKind.js
@@ -3,4 +3,13 @@ import { SameOfAKind } from './SameOfAKind.js'
 /**
  * Class representing the Four Of A Kind rule.
  */
-export class FourOfAKind extends SameOfAKind {}
+export class FourOfAKind extends SameOfAKind {
+  /**
+   * Creates an instance of FourOfAKind class.
+   */
+  constructor () {
+    const sameRankCount = 4
+
+    super(sameRankCount)
+  }
+}
