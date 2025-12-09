9f362559f8d4f436450a21080992e139253ffb84
Julia Lind <lind.julia@hotmail.com>
2025-12-09 23:09:49 +0100

    Add another unittest for Rule class RED

--- tests/rules/Rule.test.js
+++ tests/rules/Rule.test.js
@@ -5,4 +5,13 @@ describe('Rule', () => {
   it('Rule should be abstract - instantiating directly should throw an error', () => {
     expect(() => new Rule()).toThrowError(Error)
   })
+
+  it('Instantiating subclass of Rule should not throw an error', () => {
+    /**
+     * Subclass of Rule for testing purposes
+     */
+    class ConcreteRule extends Rule { }
+
+    expect(() => new ConcreteRule()).not.toThrow()
+  })
 })
