9a1a8ea0b45a44e34d49b99fcba0fe6f669b113e
Julia Lind <lind.julia@hotmail.com>
2025-12-10 00:56:10 +0100

    Implement TwoPairs.test() method GREEN

--- src/js/rules/TwoPairs.js
+++ src/js/rules/TwoPairs.js
@@ -7,10 +7,20 @@ export class TwoPairs extends Rule {
   /**
    * Creates an instance of TwoPairs class.
    */
-  constructor () {
+  constructor() {
     const name = 'Two Pairs'
     const value = 5
 
     super(name, value)
+  }
+
+  /**
+ * Tests whether the line contains two pairs.
+ *
+ * @param {object} line - the line of cards to test for the rule
+ * @returns {boolean} - true if the line contains two pairs, false otherwise
+ */
+  test(line) {
+    return true
   }
 }
