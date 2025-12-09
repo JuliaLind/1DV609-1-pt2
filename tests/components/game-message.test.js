7e81baf1955219bffb966e174ecdcf80b8765b28
Julia Lind <lind.julia@hotmail.com>
2025-12-09 20:59:55 +0100

    Add unittest for game-message component RED

---
+++ tests/components/game-message.test.js
@@ -0,0 +1,20 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import '../../src/js/components/game-message/game-message.js'
+
+describe('game-message', () => {
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    const gameMessage = document.createElement('game-message')
+
+    document.body.appendChild(gameMessage)
+  })
+
+  it('game-message should be removed from the DOM when button is clicked', () => {
+    const gameMessage = document.querySelector('game-message')
+    const button = gameMessage.shadowRoot.querySelector('button')
+    button.click()
+
+    const gameMessageAfterClick = document.querySelector('game-message')
+    expect(gameMessageAfterClick).toBeNull()
+  })
+})
