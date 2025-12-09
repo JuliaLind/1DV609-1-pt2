a3bba0fbfd3c0f8348b456b9f9c0471984b72c2a
Julia Lind <lind.julia@hotmail.com>
2025-12-09 20:58:53 +0100

    Define poker-game component GREEN

---
+++ src/js/components/poker-game/poker-game.js
@@ -0,0 +1,17 @@
+
+const template = document.createElement('template')
+
+template.innerHTML = `
+  <style>
+    :host,
+    * {
+      box-sizing: border-box;
+    }
+  </style>
+`
+
+customElements.define('poker-game',
+  /**
+   * Represents the poker-game component.
+   */
+  class extends HTMLElement {})
