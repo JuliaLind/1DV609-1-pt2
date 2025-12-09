c78285325db2a9c6074ee6988c3355479f277218
Julia Lind <lind.julia@hotmail.com>
2025-12-09 18:11:05 +0100

    Add unittest for poker-card RED

--- tests/components/poker-card.test.js
+++ tests/components/poker-card.test.js
@@ -2,7 +2,18 @@ import { describe, it, expect } from 'vitest'
 import '../../src/js/components/poker-card'
 
 describe('poker-card', () => {
-  it('poker-card should be defined as a custom element', () => {
+  it('Should be defined as a custom element', () => {
     expect(customElements.get('poker-card')).toBeDefined()
   })
+
+  it(`Alt attribute of the poker-card image should be "[Rank] of [Suite]"`, () => {
+      const sut = document.createElement('poker-card')
+
+      sut.setAttribute('rank', 'A')
+      sut.setAttribute('suite', 'Hearts')
+
+      const actual = sut.shadowRoot.querySelector('img').getAttribute('alt')
+
+      expect(actual).toEqual('A of Hearts')
+    })
 })
