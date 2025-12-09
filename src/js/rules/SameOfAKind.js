71f291b67c6068a813bdb19ed9558283a608bfdb
Julia Lind <lind.julia@hotmail.com>
2025-12-09 23:31:02 +0100

    Implement SameOfAKind GREEN

---
+++ src/js/rules/SameOfAKind.js
@@ -0,0 +1,13 @@
+/**
+ * Abstract class representing a rule that checks
+ * for a certain number of cards of the same rank.
+ */
+export class SameOfAKind {
+  /**
+   * Creates an instance of SameOfAKind.
+   *
+   */
+  constructor () {
+    throw new Error('SameOfAKind is an abstract class')
+  }
+}
