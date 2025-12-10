2371f64c1fde9771a76a3f6d37ff216afffb99d3
Julia Lind <lind.julia@hotmail.com>
2025-12-11 00:39:12 +0100

    Add first unittest for Card RED

---
+++ tests/logic/Card.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+import { Card } from '../../src/js/logic/Card.js'
+
+describe('Card', () => {
+  it('Card should be instantiated with correct rank', () => {
+    const sut = new Card('A', 'Diamonds')
+    expect(sut.rank).toBe('A')
+  })
+})
