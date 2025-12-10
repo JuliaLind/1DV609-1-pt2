945df4a01f499df7532066a4453c48ec277cc524
Julia Lind <lind.julia@hotmail.com>
2025-12-10 01:31:23 +0100

    Implement Flush.test GREEN

--- src/js/rules/Flush.js
+++ src/js/rules/Flush.js
@@ -7,10 +7,20 @@ export class Flush extends Rule {
   /**
    * Creates an instance of Flush rule.
    */
-  constructor () {
+  constructor() {
     const name = 'Flush'
     const value = 20
 
     super(name, value)
+  }
+
+  /**
+   * Tests if the hand contains five cards of same suit.
+   *
+   * @param {object} line - the line of cards to test for the rule
+   * @returns { boolean} - true if the line has five cards of the same suit, false otherwise
+   */
+  test(line) {
+    return true
   }
 }
