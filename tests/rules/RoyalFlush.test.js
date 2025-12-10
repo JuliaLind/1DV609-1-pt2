3bbfdc5b59678463bf6c3ea021a908dba573335f
Julia Lind <lind.julia@hotmail.com>
2025-12-10 15:38:21 +0100

    update test

--- tests/rules/RoyalFlush.test.js
+++ tests/rules/RoyalFlush.test.js
@@ -12,23 +12,18 @@ describe('RoyalFlush', () => {
     expect(sut.toObject()).toEqual(expected)
   })
 
-  it('RoyalFlush.test should return true if StraightFlush.test returns true and GridLine contains an Ace', () => {
+  it('RoyalFlush.test should return true if StraightFlush.test returns true and GridLine.hasRank returns true', () => {
     const straightFlushStub = vi.fn({
       test: vi.fn().mockReturnValue(true)
     })
 
     // eslint-disable-next-line
     const lineStub = vi.fn({
-      hasRank: vi.fn().mockImplementation((rank) => {
-        if (rank === 14) {
-          return true
-        }
-        return false
-      })
+      hasRank: vi.fn().mockReturnValue(true)
     })
 
     const sut = new RoyalFlush(straightFlushStub)
 
-    expect(sut.test(lineStub)).toBe(false)
+    expect(sut.test(lineStub)).toBe(true)
   })
 })
