7e0f934d5feede47c9fac8e7985d6ea6dee16b93
Julia Lind <lind.julia@hotmail.com>
2025-12-10 15:32:33 +0100

    Add first unittest for RoyalFlush.test() method RED

--- tests/rules/RoyalFlush.test.js
+++ tests/rules/RoyalFlush.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { RoyalFlush } from '../../src/js/rules/RoyalFlush.js'
 
 describe('RoyalFlush', () => {
@@ -10,5 +10,25 @@ describe('RoyalFlush', () => {
     }
 
     expect(sut.toObject()).toEqual(expected)
+  })
+
+  it('RoyalFlush.test should return true if StraightFlush.test returns true and GridLine contains an Ace', () => {
+    const straightFlushStub = vi.fn({
+      test: vi.fn().mockReturnValue(true)
+    })
+
+    // eslint-disable-next-line
+    const lineStub = vi.fn({
+      hasRank: vi.fn().mockImplementation((rank) => {
+        if (rank === 14) {
+          return true
+        }
+        return false
+      })
+    })
+
+    const sut = new RoyalFlush(straightFlushStub)
+
+    expect(sut.test(lineStub)).toBe(false)
   })
 })
