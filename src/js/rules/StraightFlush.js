2d4852412fd8980458f0ba2dd76f295a00f4dd08
Julia Lind <lind.julia@hotmail.com>
2025-12-10 09:50:49 +0100

    Adjust StraightFlush.test() GREEN

--- src/js/rules/StraightFlush.js
+++ src/js/rules/StraightFlush.js
@@ -1,24 +1,28 @@
 import { Rule } from './Rule.js'
 import { Straight } from './Straight.js'
+import { Flush } from './Flush.js'
 
 /**
  * Class representing the Straight Flush rule.
  */
 export class StraightFlush extends Rule {
   #straightRule
+  #flushRule
 
   /**
    * Creates an instance of StraightFlush class.
    *
    * @param {Straight} straightRule - an instance of the Straight rule
+   * @param {Flush} flushRule - an instance of the Flush rule
    */
-  constructor (straightRule = new Straight()) {
+  constructor (straightRule = new Straight(), flushRule = new Flush()) {
     const name = 'Straight Flush'
     const value = 75
 
     super(name, value)
 
     this.#straightRule = straightRule
+    this.#flushRule = flushRule
   }
 
   /**
@@ -28,6 +32,6 @@ export class StraightFlush extends Rule {
    * @returns { boolean } - true if both Straight and Flush rules are satisfied, false otherwise
    */
   test(line) {
-    return this.#straightRule.test(line)
+    return this.#straightRule.test(line) && this.#flushRule.test(line)
   }
 }
