1b06fdb9cb87b0f20e104ee497091a92286e8b44
Julia Lind <lind.julia@hotmail.com>
2025-12-10 18:46:09 +0100

    Add another unittest for RuleSet.evaluate() RED

--- tests/rules/RuleSet.test.js
+++ tests/rules/RuleSet.test.js
@@ -1,79 +1,27 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { RuleSet } from '../../src/js/rules/RuleSet.js'
 
+vi.mock('../../src/js/rules/Rule.js', () => {
+  return {
+    Rule: vi.fn({
+      /**
+       * Default toObject method stub.
+       *
+       * @returns {object} - associative array with empty string as name and 0 as points.
+       */
+      toObject: () => {
+        return { name: '', points: 0 }
+      }
+    })
+  }
+})
+
 describe('RuleSet', () => {
-  it('RuleSet.evaluate() should call the rules in the correct order regardless of the injection order', () => {
-    const royalFlushMock = vi.fn({
-      test: vi.fn()
-    })
-    const straightFlushMock = vi.fn({
-      test: vi.fn()
-    })
-    const fourOfAKindMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const fullHouseMock = vi.fn({
-      test: vi.fn()
-    })
-    const flushMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const straightMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const threeOfAKindMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const twoPairsMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const onePairMock = vi.fn({
-      test: vi.fn()
-    })
-
-    const sut = new RuleSet({
-      onePair: onePairMock,
-      twoPairs: twoPairsMock,
-      threeOfAKind: threeOfAKindMock,
-      royalFlush: royalFlushMock,
-      straightFlush: straightFlushMock,
-      fourOfAKind: fourOfAKindMock,
-      fullHouse: fullHouseMock,
-      straight: straightMock,
-      flush: flushMock
-    })
-
-    const lineDummy = {}
-
-    sut.evaluate(lineDummy)
-
-    const actualOrder = [
-      royalFlushMock.test.mock.invocationCallOrder[0],
-      straightFlushMock.test.mock.invocationCallOrder[0],
-      fourOfAKindMock.test.mock.invocationCallOrder[0],
-      fullHouseMock.test.mock.invocationCallOrder[0],
-      flushMock.test.mock.invocationCallOrder[0],
-      straightMock.test.mock.invocationCallOrder[0],
-      threeOfAKindMock.test.mock.invocationCallOrder[0],
-      twoPairsMock.test.mock.invocationCallOrder[0],
-      onePairMock.test.mock.invocationCallOrder[0]
-    ]
-
-    const expectedOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    expect(actualOrder).toEqual(expectedOrder)
-  })
-
   it('RuleSet.evaluate should pass the GridLine into the test() method of the rules', () => {
     const anyRuleMock = vi.fn({
       test: vi.fn()
     })
-  
+
     const sut = new RuleSet({
       onePair: anyRuleMock,
       twoPairs: anyRuleMock,
@@ -91,5 +39,84 @@ describe('RuleSet', () => {
     sut.evaluate(lineDummy)
 
     expect(anyRuleMock.test).toHaveBeenCalledWith(lineDummy)
+  })
+
+  describe('', () => {
+    const royalFlushMock = vi.fn()
+    const straightFlushMock = vi.fn()
+    const fourOfAKindMock = vi.fn()
+
+    const fullHouseMock = vi.fn()
+    const flushMock = vi.fn()
+
+    const straightMock = vi.fn()
+
+    const threeOfAKindMock = vi.fn()
+
+    const twoPairsMock = vi.fn()
+
+    const onePairMock = vi.fn()
+
+    let sut
+    beforeEach(() => {
+      royalFlushMock.test = vi.fn().mockReturnValue(false)
+      straightFlushMock.test = vi.fn().mockReturnValue(false)
+      fourOfAKindMock.test = vi.fn().mockReturnValue(false)
+      fullHouseMock.test = vi.fn().mockReturnValue(false)
+      flushMock.test = vi.fn().mockReturnValue(false)
+      straightMock.test = vi.fn().mockReturnValue(false)
+      threeOfAKindMock.test = vi.fn().mockReturnValue(false)
+      twoPairsMock.test = vi.fn().mockReturnValue(false)
+      onePairMock.test = vi.fn().mockReturnValue(false)
+
+      sut = new RuleSet({
+        onePair: onePairMock,
+        twoPairs: twoPairsMock,
+        threeOfAKind: threeOfAKindMock,
+        royalFlush: royalFlushMock,
+        straightFlush: straightFlushMock,
+        fourOfAKind: fourOfAKindMock,
+        fullHouse: fullHouseMock,
+        straight: straightMock,
+        flush: flushMock
+      })
+    })
+
+    it('RuleSet.evaluate() should call the rules in the correct order regardless of the injection order', () => {
+      const lineDummy = {}
+
+      sut.evaluate(lineDummy)
+
+      const actualOrderRaw = [
+        royalFlushMock.test.mock.invocationCallOrder[0],
+        straightFlushMock.test.mock.invocationCallOrder[0],
+        fourOfAKindMock.test.mock.invocationCallOrder[0],
+        fullHouseMock.test.mock.invocationCallOrder[0],
+        flushMock.test.mock.invocationCallOrder[0],
+        straightMock.test.mock.invocationCallOrder[0],
+        threeOfAKindMock.test.mock.invocationCallOrder[0],
+        twoPairsMock.test.mock.invocationCallOrder[0],
+        onePairMock.test.mock.invocationCallOrder[0]
+      ]
+
+      // Normalize actual order to start on 1 because invocationCallOrder counts all calls globally
+      // in the testsuite
+      const firstNr = actualOrderRaw[0]
+      const actualOrder = actualOrderRaw.map(actualNr => actualNr - firstNr + 1)
+
+      const expectedOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      expect(actualOrder).toEqual(expectedOrder)
+    })
+
+    it('RuleSet.evaluate should return default rule name and points when no rules match', () => {
+      const lineStub = {}
+      const result = sut.evaluate(lineStub)
+
+      expect(result).toEqual({
+        name: '',
+        points: 0
+      })
+    })
   })
 })
