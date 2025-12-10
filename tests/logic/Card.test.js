7ed48a76effa221245f5a194290c3207ae2af121
Julia Lind <lind.julia@hotmail.com>
2025-12-11 00:41:41 +0100

    Add another unittest for Card RED

--- tests/logic/Card.test.js
+++ tests/logic/Card.test.js
@@ -4,6 +4,13 @@ import { Card } from '../../src/js/logic/Card.js'
 describe('Card', () => {
   it('Card should be instantiated with correct rank', () => {
     const sut = new Card('A', 'Diamonds')
+
     expect(sut.rank).toBe('A')
+  })
+
+  it('Card should be instantiated with correct suite', () => {
+    const sut = new Card('A', 'Diamonds')
+
+    expect(sut.suite).toBe('Diamonds')
   })
 })
