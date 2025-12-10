40341452c0c49028c189236795c9d37f4671002a
Julia Lind <lind.julia@hotmail.com>
2025-12-10 16:24:52 +0100

    Adjust implementation of RoyalFlush.test() GREEN

--- src/js/rules/RoyalFlush.js
+++ src/js/rules/RoyalFlush.js
@@ -1,18 +1,24 @@
 import { Rule } from './Rule.js'
+import { StraightFlush } from './StraightFlush.js'
 
 /**
  * Class representing the Flush rule.
  */
 export class RoyalFlush extends Rule {
+  #straightFlushRule
+
   /**
    * Creates an instance of Royal Flush rule.
    *
+   * @param {StraightFlush} straightFlush - an instance of the StraightFlush rule
    */
-  constructor () {
+  constructor (straightFlush = new StraightFlush()) {
     const name = 'Royal Flush'
     const value = 100
 
     super(name, value)
+
+    this.#straightFlushRule = straightFlush
   }
 
   /**
@@ -22,6 +28,6 @@ export class RoyalFlush extends Rule {
    * @returns { boolean } - true if the line satisfies the Royal Flush rule, false otherwise
    */
   test (line) {
-    return line.hasRank(14)
+    return this.#straightFlushRule.test(line) && line.hasRank(14)
   }
 }
