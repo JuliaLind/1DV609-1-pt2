f7abe74eeeab13157aadff825c48908a2c07f168
Julia Lind <lind.julia@hotmail.com>
2025-12-10 09:42:26 +0100

    Implement StraightFlush.test() GREEN

--- src/js/rules/StraightFlush.js
+++ src/js/rules/StraightFlush.js
@@ -8,10 +8,20 @@ export class StraightFlush extends Rule {
    * Creates an instance of StraightFlush class.
    *
    */
-  constructor () {
+  constructor() {
     const name = 'Straight Flush'
     const value = 75
 
     super(name, value)
+  }
+
+  /**
+   * Tests whether the given line contains five cards of consecutive ranks and same suit.
+   *
+   * @param {object} line - the line of cards to test for StraightFlush rule
+   * @returns { boolean } - true if both Straight and Flush rules are satisfied, false otherwise
+   */
+  test(line) {
+    return true
   }
 }
