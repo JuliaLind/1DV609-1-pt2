97026b1cc99e220b55e39c4ec2be68a127cdefbf
Julia Lind <lind.julia@hotmail.com>
2025-12-10 01:49:59 +0100

    Implement Straight.test() GREEN

--- src/js/rules/Straight.js
+++ src/js/rules/Straight.js
@@ -7,10 +7,20 @@ export class Straight extends Rule {
   /**
    * Creates an instance of Straight rule.
    */
-  constructor () {
+  constructor() {
     const name = 'Straight'
     const value = 15
 
     super(name, value)
+  }
+
+  /**
+   * Tests whether the given line contains five cards of consecutive ranks.
+   *
+   * @param {object} line - the line of cards to test
+   * @returns { boolean } - true if the line contains five cards of consecutive ranks, false otherwise
+   */
+  test(line) {
+    return true
   }
 }
