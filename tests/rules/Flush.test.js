f3589d15ba93eb976ffd39d5bb3571b5756ba2f6
Julia Lind <lind.julia@hotmail.com>
2025-12-10 10:01:42 +0100

    REFACTOR testsuite, replace dummy objects with vproper vitest mocks

--- tests/rules/Flush.test.js
+++ tests/rules/Flush.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { Flush } from '../../src/js/rules/Flush.js'
 
 describe('Flush', () => {
@@ -14,49 +14,34 @@ describe('Flush', () => {
 
   it('Flush.test() should return true for line that has five cards of the same suit', () => {
     const sut = new Flush()
-    const lineStub = {
-      /**
-       * Stub method for getSuiteFrequencies.
-       *
-       * @returns {object} - a stubbed suit frequencies object that says there are five cards of the same suit
-       */
-      getSuiteFrequencies: () => ({
+    const lineStub = vi.fn({
+      getSuiteFrequencies: vi.fn().mockReturnValue({
         hearts: 5
       })
-    }
+    })
 
     expect(sut.test(lineStub)).toBe(true)
   })
 
   it('Flush.test() should return false for line that has five cards of two or more suites', () => {
     const sut = new Flush()
-    const lineStub = {
-      /**
-       * Stub method for getSuiteFrequencies.
-       *
-       * @returns {object} - a stubbed suit frequencies object that says there are five cards of the same suit
-       */
-      getSuiteFrequencies: () => ({
+    const lineStub = vi.fn({
+      getSuiteFrequencies: vi.fn().mockReturnValue({
         hearts: 4,
         spades: 1
       })
-    }
+    })
 
     expect(sut.test(lineStub)).toBe(false)
   })
 
   it('Flush.test() should return false for line that has four cards of same suites', () => {
     const sut = new Flush()
-    const lineStub = {
-      /**
-       * Stub method for getSuiteFrequencies.
-       *
-       * @returns {object} - a stubbed suit frequencies object that says there are five cards of the same suit
-       */
-      getSuiteFrequencies: () => ({
+    const lineStub = vi.fn({
+      getSuiteFrequencies: vi.fn().mockReturnValue({
         hearts: 4
       })
-    }
+    })
 
     expect(sut.test(lineStub)).toBe(false)
   })
