0072da55b2fad20c3d4c3e32407c468505c85287
Julia Lind <lind.julia@hotmail.com>
2025-12-10 01:49:46 +0100

    Add unittest for Straight.test() method RED

--- tests/rules/Straight.test.js
+++ tests/rules/Straight.test.js
@@ -11,4 +11,26 @@ describe('Straight', () => {
 
     expect(sut.toObject()).toEqual(expected)
   })
+
+  it('Straight.test() should return true for line with 5 unique consecutive ranks', () => {
+    const sut = new Straight()
+    const lineStub = {
+      /**
+       * Stub method for getRankFrequencies.
+       *
+       * @returns {object} - a stubbed rank frequencies object that contains five consecutive ranks
+       */
+      getRankFrequencies: () => {
+        return {
+          10: 1,
+          11: 1,
+          8: 1,
+          9: 1,
+          12: 1
+        }
+      }
+    }
+
+    expect(sut.test(lineStub)).toBe(true)
+  })
 })
