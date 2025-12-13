d9b2fd1c97d3e2455b6886643072e62649cb02bb
Julia Lind <lind.julia@hotmail.com>
2025-12-13 02:01:27 +0100

    Implement game.getNextCard() GREEN

---
+++ src/js/logic/CardDeck.js
@@ -0,0 +1,5 @@
+
+/**
+ * Placeholder for CardDeck class
+ */
+export class CardDeck {}
--- src/js/logic/Game.js
+++ src/js/logic/Game.js
@@ -1,4 +1,26 @@
+import { CardDeck } from './CardDeck.js'
+
 /**
- * Placeholder for game class.
+ * Represents the main game logic.
  */
-export class Game {}
+export class Game {
+  #cardDeck
+
+  /**
+   * Creates a new instance of Game.
+   *
+   * @param {CardDeck} cardDeck - a deck of cards
+   */
+  constructor (cardDeck = new CardDeck()) {
+    this.#cardDeck = cardDeck
+  }
+
+  /**
+   * Gets the next card to be placed.
+   *
+   * @returns {Card} - the next card to be placed
+   */
+  getNextCard () {
+    return this.#cardDeck.drawCard()
+  }
+}
