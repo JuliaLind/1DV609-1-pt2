48313189e7f5887294824a7bc4c8f2e2411c3503
Julia Lind <lind.julia@hotmail.com>
2025-12-13 13:45:04 +0100

    Implement CardDeck GREEN

--- src/js/logic/CardDeck.js
+++ src/js/logic/CardDeck.js
@@ -1,4 +1,27 @@
+import { CardFactory } from './CardFactory.js'
+
 /**
- * Placeholder for CardDeck class
+ * Represents a deck of cards.
  */
-export class CardDeck {}
+export class CardDeck {
+  #cards
+
+  /**
+   * Creates an instance of CardDeck.
+   *
+   * @param {CardFactory} cardFactory - the factory for creating cards for the deck
+   */
+  constructor(cardFactory = new CardFactory()) {
+    this.#cards = [...cardFactory.createCards()]
+  }
+
+
+  /**
+   * Picks the top card from the deck.
+   *
+   * @returns {Card} - the top card from the deck
+   */
+  drawCard () {
+    return this.#cards.pop()
+  }
+}
