0448fb97efac9dd4a57dd2d45cf79b94f23a410c
Julia Lind <lind.julia@hotmail.com>
2025-12-13 15:36:57 +0100

    Implement Grid constructor GREEN

--- src/js/logic/Grid.js
+++ src/js/logic/Grid.js
@@ -1,4 +1,13 @@
 /**
  * Placeholder for Grid class
  */
-export class Grid {}
+export class Grid {
+  /**
+   * Creates an instance of Grid with 5 grid lines.
+   *
+   * @param {GridLine[]} rows - the rows of the grid
+   */
+  constructor(rows) {
+    throw new Error('Grid must have 5 rows')
+  }
+}
