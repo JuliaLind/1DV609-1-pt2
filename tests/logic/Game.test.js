10fc6ae17794f47ed79b7cc0a8b3b87a168389c8
Julia Lind <lind.julia@hotmail.com>
2025-12-13 03:27:20 +0100

    Add another unittest for placeCardAt RED

--- tests/logic/Game.test.js
+++ tests/logic/Game.test.js
@@ -2,7 +2,7 @@ import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
 import { Game } from '../../src/js/logic/Game.js'
 
 describe('Game', () => {
-  const cardDeckMock = vi.fn({
+  const cardDeckStub = vi.fn({
     drawCard: vi.fn()
   })
 
@@ -22,8 +22,14 @@ describe('Game', () => {
     placeCard: vi.fn()
   })
 
+  const ruleSetMock = vi.fn(
+    {
+      evaluate: vi.fn()
+    }
+  )
+
   beforeEach(() => {
-    cardDeckMock.drawCard
+    cardDeckStub.drawCard
       .mockClear()
       .mockReturnValueOnce(card1)
       .mockReturnValueOnce(card2)
@@ -35,21 +41,21 @@ describe('Game', () => {
   })
 
   it('Game.getNextCard() should retrieve the next card from the deck', () => {
-    const sut = new Game(cardDeckMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
     const actualCard = sut.getNextCard()
 
     expect(actualCard).toBe(card1)
   })
 
   it('Game.getNextCard() should not pick a new card from the deck on each call', () => {
-    const sut = new Game(cardDeckMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
     const firstCard = sut.getNextCard()
     const secondCard = sut.getNextCard()
     expect(secondCard).toBe(firstCard)
   })
 
   it('After placeCardAt() has been called the next card should be picked from the deck', () => {
-    const sut = new Game(cardDeckMock, gridMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
 
     const rowIndex = 1
     const columnIndex = 3
@@ -59,7 +65,7 @@ describe('Game', () => {
   })
 
   it('Game.placeCardAt() should place the current card on the grid at specified position', () => {
-    const sut = new Game(cardDeckMock, gridMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
 
     const rowIndex = 1
     const columnIndex = 3
@@ -70,17 +76,28 @@ describe('Game', () => {
 
 
   it('Game.getResult() should return { name: "", points: 0 } as default value', () => {
-    const sut = new Game(cardDeckMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
     const expected = { name: '', points: 0 }
     const actual = sut.getResult('row', 2)
 
     expect(actual).toEqual(expected)
   })
 
+  it('Game.placeCardAt() should evaluate and store the result for the affected row', () => {
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
+    const expectedRowResult = { rule: 'SomeRule', points: 10 }
+    ruleSetMock.evaluate
+        .mockReset()
+        .mockReturnValueOnce(expectedRowResult)
+
+    sut.placeCardAt(1, 3)
+    expect(sut.getResult('row', 1)).toEqual(expectedRowResult)
+  })
+
   it(`Game.isOver() should return true if the grid isFull() returns true`, () => {
     gridMock.isFull.mockReturnValue(true)
 
-    const sut = new Game(cardDeckMock, gridMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
 
     const actual = sut.isOver()
 
@@ -90,7 +107,7 @@ describe('Game', () => {
   it(`Game.isOver() should return false if the grid isFull() returns false`, () => {
     gridMock.isFull.mockReturnValue(false)
 
-    const sut = new Game(cardDeckMock, gridMock)
+    const sut = new Game(cardDeckStub, gridMock, ruleSetMock)
 
     const actual = sut.isOver()
 
