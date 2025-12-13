896cb5e14b69503a2d6c79f4cde3d6356a3a0cb8
Julia Lind <lind.julia@hotmail.com>
2025-12-13 14:52:22 +0100

    REFACTOR move code from constructor to maller metthods

--- src/js/logic/CardDeck.js
+++ src/js/logic/CardDeck.js
@@ -11,16 +11,40 @@ export class CardDeck {
    *
    * @param {CardFactory} cardFactory - the factory for creating cards for the deck
    */
-  constructor(cardFactory = new CardFactory()) {
+  constructor (cardFactory = new CardFactory()) {
     this.#cards = [...cardFactory.createCards()]
+    this.#shuffleInPlace()
+  }
 
+  /**
+   * Shuffles the cards in the deck in place using the Fisher-Yates algorithm.
+   */
+  #shuffleInPlace () {
     for (let index = this.#cards.length - 1; index > 0; index--) {
-      const randomIndex = Math.floor(Math.random() * (index + 1))
+      const randomIndex = this.#selectRandomIndex(index + 1)
 
-      const temp = this.#cards[index]
-      this.#cards[index] = this.#cards[randomIndex]
-      this.#cards[randomIndex] = temp
+      this.#swapCards(index, randomIndex)
     }
+  }
+
+  /**
+   * Selects a random index up to the given upper limit.
+   *
+   * @param {number} upperLimit - the upper limit (exclusive) for the random index
+   * @returns {number} - the selected random index
+   */
+  #selectRandomIndex (upperLimit) {
+    return Math.floor(Math.random() * upperLimit)
+  }
+
+  /**
+   * Swaps two cards in the deck.
+   *
+   * @param {number} index1 - index position of first card
+   * @param {number} index2 - index position of second card
+   */
+  #swapCards (index1, index2) {
+    [this.#cards[index1], this.#cards[index2]] = [this.#cards[index2], this.#cards[index1]]
   }
 
   /**
