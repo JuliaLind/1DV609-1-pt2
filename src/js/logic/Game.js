4a626331ee927794e74a68b768b5eeacc9243e67
Julia Lind <lind.julia@hotmail.com>
2025-12-13 02:16:09 +0100

    Implement Game.getResult() GREEN

--- src/js/logic/Game.js
+++ src/js/logic/Game.js
@@ -4,25 +4,36 @@ import { CardDeck } from './CardDeck.js'
  * Represents the main game logic.
  */
 export class Game {
-  #cardDeck
-  #nextCard
+    #cardDeck
+    #nextCard
 
-  /**
-   * Creates a new instance of Game.
-   *
-   * @param {CardDeck} cardDeck - a deck of cards
-   */
-  constructor (cardDeck = new CardDeck()) {
-    this.#cardDeck = cardDeck
-    this.#nextCard = cardDeck.drawCard()
-  }
+    /**
+     * Creates a new instance of Game.
+     *
+     * @param {CardDeck} cardDeck - a deck of cards
+     */
+    constructor(cardDeck = new CardDeck()) {
+        this.#cardDeck = cardDeck
+        this.#nextCard = cardDeck.drawCard()
+    }
 
-  /**
-   * Gets the next card to be placed.
-   *
-   * @returns {Card} - the next card to be placed
-   */
-  getNextCard () {
-    return this.#nextCard
-  }
+    /**
+     * Gets the next card to be placed.
+     *
+     * @returns {Card} - the next card to be placed
+     */
+    getNextCard() {
+        return this.#nextCard
+    }
+
+    /**
+     * Returns the result for the specified row.
+     *
+     * @param {string} direction - the direction of line to get results for
+     * @param {number} index - index of the row/column to get results for
+     * @returns {object} - result object for the specified row
+     */
+    getResult(direction, index) {
+        return { name: '', points: 0 }
+    }
 }
