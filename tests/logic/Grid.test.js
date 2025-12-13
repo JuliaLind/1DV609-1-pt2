9b961ea61a353914cd943c3a96613d49daaf2f66
Julia Lind <lind.julia@hotmail.com>
2025-12-13 15:36:42 +0100

    Add unittest for Grid constructor RED

---
+++ tests/logic/Grid.test.js
@@ -0,0 +1,30 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { Grid } from '../../src/js/logic/Grid.js'
+
+describe('Grid', () => {
+  const card1 = { rank: 'A', suite: 'Hearts' }
+  const card2 = { rank: 'K', suite: 'Spades' }
+  const card3 = { rank: '10', suite: 'Diamonds' }
+
+  let slots1
+  let slots2
+  let slots3
+  let slots4
+  let slots5
+
+  beforeEach(() => {
+    slots1 = [undefined, card1, undefined, undefined, undefined]
+    slots2 = [undefined, undefined, undefined, undefined, undefined]
+    slots3 = [undefined, undefined, card2, undefined, undefined]
+    slots4 = [card3, undefined, undefined, undefined, undefined]
+    slots5 = [undefined, undefined, undefined, undefined, undefined]
+  })
+
+
+  it('constructor should throw error if less than 5 rows are passed into constructor', () => {
+    const fourRows = [{}, {}, {}, {}]
+
+    expect(() => new Grid(fourRows)).toThrowError()
+  })
+  
+})
