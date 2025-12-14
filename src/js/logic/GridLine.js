0b7f60a805564b21118ead0aa38a86570f76eeab
Julia Lind <lind.julia@hotmail.com>
2025-12-14 23:11:33 +0100

    Implement GridLine constructor GREEN

--- src/js/logic/GridLine.js
+++ src/js/logic/GridLine.js
@@ -1,4 +1,24 @@
 /**
  * Plaeholder for GridLine class
  */
-export class GridLine {}
+export class GridLine {
+  #slots
+
+  /**
+   * Creates a new line for the grid with 5 empty slots.
+   *
+   */
+  constructor() {
+
+    this.#slots = new Array(5)
+  }
+
+  /**
+   * Returns a shallow copy of the slots in the grid line.
+   *
+   * @returns {Array} - array of slots in the grid line
+   */
+  get slots() {
+    return [...this.#slots]
+  }
+}
