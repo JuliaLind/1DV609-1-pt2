6abfc5d397fdc9ac12f10c138417f31fbc5fa5b2
Julia Lind <lind.julia@hotmail.com>
2025-12-14 15:15:33 +0100

    Implement getRow GREEN

--- src/js/logic/Grid.js
+++ src/js/logic/Grid.js
@@ -4,6 +4,8 @@ import { GridLine } from './GridLine.js'
  * Placeholder for Grid class
  */
 export class Grid {
+  #rows
+
   /**
    * Creates an instance of Grid with 5 grid lines.
    *
@@ -19,5 +21,17 @@ export class Grid {
     if (rows.length !== 5) {
       throw new Error('Grid must have 5 rows')
     }
+
+    this.#rows = [...rows]
+  }
+
+  /**
+   * Returns the row at the specified index.
+   *
+   * @param {number} index - the index of the row to retrieve
+   * @returns {GridLine} - the grid line at the specified index
+   */
+  getRow(index) {
+    return new GridLine(this.#rows[index].slots)
   }
 }
